2a6a1ea2470aef1425c1193173b1bdd9fd012ba5
Guillermo Verón <guiver95@gmail.com>
2022-08-22 20:51:20 -0400

    first commit

---
+++ BuberBreakfast.Contracts/Breakfasts/UpsertBreakfastRequest.cs
@@ -0,0 +1,10 @@
+namespace BuberBreakfast.Contracts.Breakfasts
+{
+  public record UpsertBreakfastRequest(
+    string Name,
+    string Description,
+    DateTime StartDateTime,
+    DateTime EndDateTime,
+    List<string> Savory,
+    List<string> Sweet);
+}
